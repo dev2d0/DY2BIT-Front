e01062e24f4265b77e06db8beb99cac16aa2767c
dev2d0 <leedy2491@ajou.ac.kr>
2022-05-08 17:50:11 +0900

    Fix: 환경 변수 설정

--- lib/utils/runtimeConfig.ts
+++ lib/utils/runtimeConfig.ts
@@ -10,12 +10,16 @@ interface RuntimeConfig {
 export function getRuntimeConfig(): RuntimeConfig {
   const runtimeConfig = getConfig()?.publicRuntimeConfig
   if (runtimeConfig) {
-    return runtimeConfig
+    return {
+      HOST: 'http://ec2-15-165-167-225.ap-northeast-2.compute.amazonaws.com:8080/',
+      PRODUCTION: 'true',
+      IS_TEST_ENV: 'false',
+    }
   }
   // FIXME: 실제 사용할 value 로 수정
   return {
     HOST: 'http://ec2-15-165-167-225.ap-northeast-2.compute.amazonaws.com:8080/',
-    PRODUCTION: 'false',
-    IS_TEST_ENV: 'true',
+    PRODUCTION: 'true',
+    IS_TEST_ENV: 'false',
   }
 }
